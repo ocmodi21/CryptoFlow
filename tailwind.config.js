30d0305b3be6749c40ebedb27a6f81f78c3c8e2d
ocmodi21 <ocmodi2117@gmail.com>
2024-07-19 00:09:17 +0530

    chore: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      dark: {
+        bgcolor: {
+          primary: "#1d1d1d",
+          secondary: "#333333",
+        },
+        font: {
+          primary: "#e4e2e3",
+          secondary: "#818285",
+        },
+      },
+      light: {
+        bgcolor: {
+          primary: "#fff",
+          secondary: "#f6f6f6",
+        },
+        font: {
+          primary: "#16171a",
+          secondary: "#0151c8",
+        },
+      },
+      main: "#d7fd63",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
